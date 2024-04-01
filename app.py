4abe616dc43387584a5975e622a7e714b61dbc0b
VidhiShah195 <shahvidhi195@gmail.com>
2024-04-01 18:56:06 -0400

    update to penguins

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt 
 import seaborn as sns
+
 st.title("Palmer's Penguins")
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 penguin_file = st.file_uploader(
