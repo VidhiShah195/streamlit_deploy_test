6de46a316fb908b1112d08dbd63a32b3eb0c7665
VidhiShah195 <shahvidhi195@gmail.com>
2024-04-01 18:42:19 -0400

    change to penguins

--- app.py
+++ app.py
@@ -1,3 +1,98 @@
 import streamlit as st
+import pandas as pd
+st.title("Palmer's Penguins")
+#import our data
+penguins_df = pd.read_csv('penguins.csv')
+st.write(penguins_df.head())
 
-st.write('Hello')
+
+import streamlit as st
+import pandas as pd
+import altair as alt
+import seaborn as sns
+st.title("Palmer's Penguins")
+st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
+selected_species = st.selectbox('What species would you like to visualize?',
+ ['Adelie', 'Gentoo', 'Chinstrap'])
+selected_x_var = st.selectbox('What do you want the x variable to be?',['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?',['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+
+
+import streamlit as st
+import pandas as pd
+import altair as alt
+import seaborn as sns
+st.title("Palmer's Penguins")
+st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
+selected_species = st.selectbox('What species would you like to visualize?', ['Adelie', 'Gentoo', 'Chinstrap'])
+selected_x_var = st.selectbox('What do you want the x variable to be?',['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?', ['bill_depth_mm', 'bill_length_mm', 'flipper_length_mm', 'body_mass_g'])
+penguins_df = pd.read_csv('penguins.csv')
+penguins_df = penguins_df[penguins_df['species'] == selected_species] 
+alt_chart = (
+ alt.Chart(penguins_df)
+ .mark_circle()
+ .encode(
+ x=selected_x_var,
+ y=selected_y_var,
+ )
+)
+st.altair_chart(alt_chart)
+
+
+import altair as alt
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+st.title("Palmer's Penguins")
+st.markdown("Use this Streamlit app to make your own scatterplot about penguins!")
+selected_species = st.selectbox(
+ "What species would you like to visualize?", ["Adelie", "Gentoo", 
+"Chinstrap"]
+)
+selected_x_var = st.selectbox(
+ "What do you want the x variable to be?",["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
+)
+selected_y_var = st.selectbox(
+ "What about the y?",
+ ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
+)
+penguins_df = pd.read_csv("penguins.csv")
+penguins_df = penguins_df[penguins_df["species"] == selected_species]
+alt_chart = (alt.Chart(penguins_df, title=f"Scatterplot of {selected_species} Penguins")
+ .mark_circle()
+ .encode(
+ x=selected_x_var,
+ y=selected_y_var, )
+)
+st.altair_chart(alt_chart)
+
+
+import altair as alt
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+st.title("Palmer's Penguins")
+st.markdown("Use this Streamlit app to make your own scatterplot about penguins!")
+selected_species = st.selectbox(
+ "What species would you like to visualize?", ["Adelie", "Gentoo", 
+"Chinstrap"]
+)
+selected_x_var = st.selectbox(
+ "What do you want the x variable to be?",["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
+)
+selected_y_var = st.selectbox(
+ "What about the y?", ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
+)
+penguins_df = pd.read_csv("penguins.csv")
+penguins_df = penguins_df[penguins_df["species"] == selected_species]
+alt_chart = ( alt.Chart(penguins_df, title=f"Scatterplot of {selected_species}Penguins")
+ .mark_circle()
+Chapter 2 31
+ .encode(
+ x=selected_x_var,
+ y=selected_y_var,
+ )
+ .interactive()
+)
+st.altair_chart(alt_chart, use_container_width=True)
